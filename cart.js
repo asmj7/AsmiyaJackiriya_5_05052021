ba829c24994542f290bc61b4a4f3e30c5f7d5450
Asmiya Jackiriya <asmiyaj77@gmail.com>
2021-05-05 17:06:50 +0200

    LocalStorage

--- cart.js
+++ cart.js
@@ -3,7 +3,6 @@ const url2 = 'http://localhost:3000/api/teddies/';
 const urlParams2 = new URLSearchParams(window.location.search);
 const id2 = urlParams2.get('id');
 
-// document.querySelector('.btnAdd').addEventListener("click", getSelectedTeddies);
 
 function getSelectedTeddies(teddy) {
 
@@ -13,7 +12,33 @@ function getSelectedTeddies(teddy) {
   let price = document.querySelector('.p3-price')
   price.innerText = `${(teddy.price/100).toFixed(2)+' €'}`;
 
+
+  //Récuperer le produit sélectionné dns le localStorage
+  const btnAdd = document.querySelector('.btnAdd');
+  let items = []
+  for(i=0; i<btnAdd.length; i++) {
+    btnAdd[i].addEventListener("click",function(e){
+      if(typeof(Storage) !== 'undefined'){
+    
+        let item = {
+          _id: teddy._id,
+          name: teddy.imageUrl,
+          price: teddy.price
+        }
+
+      }else {
+        console.log('Erreur')
+      }
+    })
+  }
+
+  
 }
+
+// function incrementCart() {}
+
+// function decrementCart() {}
+
 
 fetch(url2 + id2)
   .then(function (response) {
